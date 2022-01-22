b37f0879338e8ae47b76b1d556cf95ddb567a9ec
Leonardo Puebla <leonardo38361@gmail.com>
2022-01-22 01:14:39 -0300

    Updates

---
+++ precache-manifest.a65ebc669345273f8ae203923ab6c573.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9597a97b3f87b1765b7acc68b44e47dd",
+    "url": "/TODO-MACHINE/index.html"
+  },
+  {
+    "revision": "548beb5566ac47c6716b",
+    "url": "/TODO-MACHINE/static/css/main.3f32b903.chunk.css"
+  },
+  {
+    "revision": "dfa69edea5e6cc6f2a47",
+    "url": "/TODO-MACHINE/static/js/2.90272c86.chunk.js"
+  },
+  {
+    "revision": "548beb5566ac47c6716b",
+    "url": "/TODO-MACHINE/static/js/main.504e8ce2.chunk.js"
+  },
+  {
+    "revision": "41d6db114e04bb235b38",
+    "url": "/TODO-MACHINE/static/js/runtime~main.9d260e99.js"
+  },
+  {
+    "revision": "dcd86218bce99b2d3c3e847ba9102909",
+    "url": "/TODO-MACHINE/static/media/confirmar.dcd86218.svg"
+  },
+  {
+    "revision": "1d10709d9c98e221bc841d3046b1efc5",
+    "url": "/TODO-MACHINE/static/media/eliminar.1d10709d.svg"
+  }
+]);
